1b31782c6e230f993a9efe36fe5d706f915cda7a
이희웅 <leeheewoong@ihuiung-ui-MacBook-Pro.local>
2018-07-09 11:36:39 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+using System.Text.RegularExpressions;
+
+namespace Ch04_RegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("너의 나이를 입력해라 : ");
+            string input = ReadLine();
+            Regex ageChecker = new Regex(@"\d");
+            if(ageChecker.IsMatch(input))
+            {
+                WriteLine("잘했다!");
+            }
+            else
+            {
+                WriteLine($"아냐,아냐 {input} 이건 입력방식이 아니야");
+            }
+        }
+    }
+}
